46057053b51689b8166089b1a1f3f3cae85ad0a7
Andrea Gulli <andreagulli11@gmail.com>
2024-10-21 18:04:34 +0200

    add BONUS 1 & fix NAV Root

---
+++ resources/views/main.blade.php
@@ -0,0 +1,2 @@
+<a href="http://127.0.0.1:8000/nav">Vai a Nav</a>
+<a href="http://127.0.0.1:8000/">Torna a Home</a>
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/main', function () {
+    return view('main');
+});
+
+
 Route::get('/', function () {
     return view('home');
 });
 
 
-Route::get('/', function () {
+Route::get('/nav', function () {
     return view('nav');
 });
+
